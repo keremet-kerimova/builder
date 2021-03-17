b598e43360139b93bcab9584129ea161a970ecad
Your Name <you@example.com>
2021-03-17 16:04:45 +0600

    Layout.css

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import classes from "./Layout.module.css"
+import Toolbar from "../Toolbar/Toolbar"
+import Drawer from "../Drawer.js/Drawer"
+
+const Layout = ({children}) => {
+    return(
+        <div className ={classes.Layout}>
+            <Toolbar />
+            <Drawer />
+            {children}
+
+        </div>
+    );
+}
+export default Layout;
