d95f8c173d48444773fde412822713479d39f5e4
Your Name <you@example.com>
2021-03-17 16:04:19 +0600

    Drawer

---
+++ src/components/Drawer.js/Drawer.js
@@ -0,0 +1,13 @@
+import Logo from "../Logo/Logo"
+import Nav from  "../Nav/Nav"
+import classes from "./Drawer.module.css"
+
+const Drawer = ({ }) => {
+    return (
+        <div className={classes.Drawer}>
+            <Logo />
+            <Nav />
+        </div>
+    );
+}
+ export default Drawer
