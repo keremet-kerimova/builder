727f789ec7fa0d6b45a82da8e17afe51873b7dd8
Your Name <you@example.com>
2021-03-17 16:06:35 +0600

    my secound cp e

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import classes from "./Nav.module.css" 
+
+const Nav = ({children}) => {
+    return(
+        <div className ={classes.Nav}>
+            Nav
+
+        </div>
+    );
+}
+export default Nav;
