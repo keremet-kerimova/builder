94ae33219b732d43e9781461077c3eba62c0158f
Your Name <you@example.com>
2021-03-17 16:06:51 +0600

    Toolbar

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,13 @@
+import Logo from "../Logo/Logo"
+import Nav from "../Nav/Nav"
+import classes from "./Toolbar.module.css"
+
+const Toolbar = ({}) => {
+    return (  
+        <div className={classes.Toolbar}>
+         <Logo />
+         <Nav />
+        </div>
+    );
+}
+export default Toolbar
