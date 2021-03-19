e19d1f0bda7ce70a3b1a25b26c2c8dfd0f31e222
Your Name <you@example.com>
2021-03-19 15:25:43 +0600

    changing

--- src/components/Drawer.js/Drawer.js
+++ src/components/Drawer.js/Drawer.js
@@ -2,12 +2,21 @@ import Logo from "../Logo/Logo"
 import Nav from  "../Nav/Nav"
 import classes from "./Drawer.module.css"
 import Backdrop from "../Ul/Backdrop/Backdrop"
+import { useState } from "react"
 
 const Drawer = () => {
+ const [drawerOpen, setDrawerOpen]= useState(false);
+
+  const drawerClasses =[
+      classes.content,
+      drawerOpen ? classes.open : classes.close
+  ];
+  
+
     return (
         <div className={classes.Drawer}>
-            <Backdrop />
-            <div className={[classes.content, classes.close].join(" ")}/>
+            <Backdrop show={} />
+            <div className={drawerClasses.join(" ")}/>
             <Logo />
             <Nav />
             
