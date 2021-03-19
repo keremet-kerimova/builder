1228d6c24dbd17e7bf9ad1bc23f6551ca820f41d
Your Name <you@example.com>
2021-03-19 14:26:28 +0600

    added logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,11 @@
 import classes from "./Logo.module.css" 
+import logo from "../../images/logo1.svg"
+
 
 const Logo = () => {
     return(
         <div className ={classes.Logo}>
-            Logo
+        <img src={logo} alt="Pizza"></img>
         </div>
     );
 }
