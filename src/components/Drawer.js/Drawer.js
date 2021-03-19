d91df5815b2fa91ebd46133476908d16d0221310
Your Name <you@example.com>
2021-03-19 14:07:32 +0600

    added drawers code

--- src/components/Drawer.js/Drawer.js
+++ src/components/Drawer.js/Drawer.js
@@ -2,7 +2,7 @@ import Logo from "../Logo/Logo"
 import Nav from  "../Nav/Nav"
 import classes from "./Drawer.module.css"
 
-const Drawer = ({ }) => {
+const Drawer = () => {
     return (
         <div className={classes.Drawer}>
             <Logo />
