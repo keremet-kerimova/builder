35f738a57715236695ed27cec77675586599dfca
Your Name <you@example.com>
2021-03-19 14:08:19 +0600

    added code

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -2,7 +2,7 @@ import Logo from "../Logo/Logo"
 import Nav from "../Nav/Nav"
 import classes from "./Toolbar.module.css"
 
-const Toolbar = ({}) => {
+const Toolbar = () => {
     return (  
         <div className={classes.Toolbar}>
          <Logo />
