cdd570444469bf2de764a14ee72b581aa8765667
Your Name <you@example.com>
2021-03-19 14:56:28 +0600

    Drowers code

--- src/components/Drawer.js/Drawer.js
+++ src/components/Drawer.js/Drawer.js
@@ -1,12 +1,14 @@
 import Logo from "../Logo/Logo"
 import Nav from  "../Nav/Nav"
 import classes from "./Drawer.module.css"
+import Backdrop from "../Ul/Backdrop/Backdrop"
 
 const Drawer = () => {
     return (
         <div className={classes.Drawer}>
             <Logo />
             <Nav />
+            <Backdrop />
         </div>
     );
 }
