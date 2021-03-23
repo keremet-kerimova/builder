5c4e90893f18d31ba2bf669f3b6a2e136aa13c38
Your Name <you@example.com>
2021-03-23 17:01:20 +0600

    added js Toolbar code

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,4 +1,4 @@
-import DrawerOpen from "../Drawer/DrawerOpen/DrawerOpen";
+import DrawerOpen from "../Drawer.js/DrawerOpen/DrawerOpen";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
 import classes from "./Toolbar.module.css";
