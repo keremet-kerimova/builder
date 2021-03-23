756e36b3682bc0187b8a275c8dc0727c42136e78
Your Name <you@example.com>
2021-03-23 17:00:56 +0600

    Layout codde changing for css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import classes from "./Layout.module.css";
 import Toolbar from "../Toolbar/Toolbar";
-import Drawer from "../Drawer/Drawer";
+import Drawer from "../Drawer.js/Drawer";
 
 const Layout = ({ children }) => {
   const [drawerOpen, setDrawerOpen] = useState(false);
