368414abe3970ad945cea0d59f91726d54b23d28
Your Name <you@example.com>
2021-03-23 18:23:41 +0600

    changinh text

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,8 +6,8 @@ import logo from "../../images/logo1.svg"
 const Logo = () => {
     return(
         <div className ={classes.Logo}>
-        <img src={logo} alt=" Logo of the Pizza project"/>
-        <div>Pizza</div>
+        <img src={logo} alt=" Logo of the Dried fruits project"/>
+        <div>Dried fruits</div>
         </div>
     );
 }
