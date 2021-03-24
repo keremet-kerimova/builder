a28a918aa07be45924c5f03f3b8ed3e46fae663c
Your Name <you@example.com>
2021-03-24 14:39:59 +0600

    js file s

---
+++ src/components/DriedFruitsBuilder/DriedFruitsIngredient/DriedFruitsIngredient/DriedFruitsIngredient/DriedFruitsIngredient.js
@@ -0,0 +1,8 @@
+//  import classes from "./DriedfruitsIngredient.module.css"
+ 
+ const DriedfruitsIngredient =({ type }) => {
+     return(
+         <div >{type}</div>
+     );
+ }
+  export default DriedfruitsIngredient;
