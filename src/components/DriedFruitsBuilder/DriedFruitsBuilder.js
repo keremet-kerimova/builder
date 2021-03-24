d18fea319f49d6c60f10d8475e15350aca0e9004
Your Name <you@example.com>
2021-03-24 14:37:25 +0600

    added new files

---
+++ src/components/DriedFruitsBuilder/DriedFruitsBuilder.js
@@ -0,0 +1,11 @@
+import classes from "./DriedFruitsBuilder.module.css" 
+
+const DriedFruitsBuilder = ({children}) => {
+    return(
+        <div className ={classes.DriedFruitsBuilder}>
+            Nav
+
+        </div>
+    );
+}
+export default DriedFruitsBuilder;
