5d4d4c68ff49b5ab387425b93065a89cf14305da
Your Name <you@example.com>
2021-03-25 22:23:15 +0600

    js code for Controls

---
+++ src/components/DriedFruitsBuilder/DriedFruitsControls/DriedFruitsControls.js
@@ -0,0 +1,9 @@
+import classes from "./DriedFruitsControls.module.css";
+
+const DriedFruitsControls = () => {
+  return (
+    <div className={classes.DriedFruitsControls}>Controls</div>
+  );
+}
+
+export default DriedFruitsControls;
