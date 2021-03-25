da28933301050ee335987dbcf379977fec9287a0
Your Name <you@example.com>
2021-03-25 22:23:02 +0600

    code fo controls

--- src/components/DriedFruitsBuilder/DriedFruitsBuilder.js
+++ src/components/DriedFruitsBuilder/DriedFruitsBuilder.js
@@ -1,11 +1,18 @@
 import classes from "./DriedFruitsBuilder.module.css" 
+import DriedFruitsPreview from "./DriedFruitsPreview/DriedFruitsPreveiws";
 
-const DriedFruitsBuilder = ({children}) => {
+const DriedFruitsBuilder = () => {
+    const ingredients = {
+        kiwi:1,
+        nuts:2,
+        raisins:3,
+    };
     return(
         <div className ={classes.DriedFruitsBuilder}>
-            Nav
+         <DriedFruitsPreview ingredients={ingredients} />
+         <DriedFruitsControl />
 
         </div>
     );
 }
-export default DriedFruitsBuilder;
+export default DriedFruitsBuilder;   
