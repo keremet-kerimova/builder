3c7a8ec064d4aee22b9f0f85cfb7b98326352bb3
Your Name <you@example.com>
2021-03-26 15:06:50 +0600

    Controls code chnaging

--- src/components/DriedFruitsBuilder/DriedFruitsControls/DriedFruitsControls.js
+++ src/components/DriedFruitsBuilder/DriedFruitsControls/DriedFruitsControls.js
@@ -1,8 +1,14 @@
+import Control from "./Control/Control";
 import classes from "./DriedFruitsControls.module.css";
 
-const DriedFruitsControls = () => {
+const DriedFruitsControls = ({ingredients}) => {
+  const result = [];
+  for (const ingredient in ingredients) {
+    result.push(<Control type={ingredient}/>)
+    
+    }
   return (
-    <div className={classes.DriedFruitsControls}>Controls</div>
+    <div className={classes.DriedFruitsControls}>{result}</div>
   );
 }
 
