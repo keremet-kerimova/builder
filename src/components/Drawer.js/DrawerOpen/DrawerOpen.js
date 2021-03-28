659cef449eb0435ad78756baafb09a510b3445cb
Your Name <you@example.com>
2021-03-28 22:03:20 +0600

    chnaging Opens

--- src/components/Drawer.js/DrawerOpen/DrawerOpen.js
+++ src/components/Drawer.js/DrawerOpen/DrawerOpen.js
@@ -2,7 +2,11 @@ import classes from "./DrawerOpen.module.css";
 
 const DrawerOpen = ({ click }) => {
   return (
-    <div className={classes.DrawerOpen} onClick={click}>Drawer</div>
+    <div className={classes.DrawerOpen} onClick={click}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   );
 }
 
