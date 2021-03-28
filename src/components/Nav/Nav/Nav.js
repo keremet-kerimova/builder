eb49bbb7aa23c86e333c02d9d53d48ab49795ec3
Your Name <you@example.com>
2021-03-28 22:36:33 +0600

    commitys

--- src/components/Nav/Nav/Nav.js
+++ src/components/Nav/Nav/Nav.js
@@ -1,18 +1,9 @@
-import React from "react";
-import NavItem from "./NavItem/NavItem";
 import classes from "./Nav.module.css";
-import { useSelector } from "react-redux";
 
-export default () => {
-  const isAuthenticated = useSelector((state) => state.auth.token !== null);
+const Nav = () => {
   return (
-    <div className={classes.Nav}>
-      <ul>
-        <NavItem url="/builder"> Cake Builder</NavItem>
-        {isAuthenticated ? <NavItem url="/orders">Orders</NavItem> : null}
-        {!isAuthenticated ? <NavItem url="/auth">Authenticate</NavItem> : null}
-        {isAuthenticated ? <NavItem url="/logout">Logout</NavItem> : null}
-      </ul>
-    </div>
+    <div className={classes.Nav}>Nav</div>
   );
-};
+}
+
+export default Nav;
