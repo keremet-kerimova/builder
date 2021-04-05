6af32bc61b10433a063d8a679aa4e59b7d846d6a
Your Name <you@example.com>
2021-04-05 16:28:50 +0600

    added code bulders code

--- src/components/DriedFruitsBuilder/Builder/DriedFruitsBuilder.js
+++ src/components/DriedFruitsBuilder/Builder/DriedFruitsBuilder.js
@@ -2,47 +2,52 @@ import classes from "./DriedFruitsBuilder.module.css";
 import DriedFruitsPreviews from "../DriedFruitsPreviews/DriedFruitsPreviews";
 import DriedFruitsControls from "../DriedFruitsControls/Controls/DriedFruitsControls";
 import { useEffect, useState } from "react";
-
-
+import axios from "axios"
 
 const DriedFruitsBuilder = () => {
-  const [ingredients, setIngredients] = useState ({
-    tomato: 20,
-    salami: 20,
-    greenOlive: 20,
-    blackOlive: 20,
-    redPepper: 20,
-    yellowPepper: 15,
+  const prices = {
+    tomato: 5,
+    salami: 4,
+    blackOlive: .5,
+    greenOlive: .5,
+    redPepper: 3,
+    yellowPepper: 2,
+  };
+  const [ingredients, setIngredients] = useState({
+    tomato: 1,
+    salami: 1,
+    greenOlive: 1,
+    blackOlive: 1,
+    redPepper: 1,
+    yellowPepper: 1,
   });
+  const [price, setPrice] = useState(150);
 
-  function addIngredient(type){
-   const newIngredients = { ...ingredients };
-   newIngredients[type]++;
-   setIngredients(newIngredients);
+  function addIngredient(type) {
+    const newIngredients = { ...ingredients };
+    newIngredients[type]++;
+    setPrice(price + prices[type]);
+    setIngredients(newIngredients);
   }
 
-  function removeIngredient(type){
-    const newIngredients = { ...ingredients };
-    newIngredients[type]--;
-    setIngredients(newIngredients);
-   }
+  function removeIngredient(type) {
+    if (ingredients[type]) {
+      const newIngredients = { ...ingredients };
+      newIngredients[type]--;
+      setPrice(price - prices[type]);
+      setIngredients(newIngredients);
+    }
+  }
 
-  useEffect(() => {
-    axios.get('https://builder-6d74a-default-rtdb.firebaseio.com/ingredients.json'
-      .then((response) => {
-        console.log(response);
-        const email = response.data.email;
-        setEmail(email);
-      }), [userId]);
-  
 
   return (
     <div className={classes.DriedFruitsBuilder}>
-      <DriedFruitsPreviews ingredients={ingredients} />
-      <DriedFruitsControls  ingredients={ingredients}  addIngredient = {addIngredient} removeIngredient ={removeIngredient} />
-    </div>
-  );
-}
+    <DriedFruitsPreviews price={price} ingredients={ingredients} />
+    <DriedFruitsControls
+      ingredients={ingredients}
+      addIngredient={addIngredient}
+      removeIngredient={removeIngredient}
+      />
+  </div>
 
 export default DriedFruitsBuilder;
- 
