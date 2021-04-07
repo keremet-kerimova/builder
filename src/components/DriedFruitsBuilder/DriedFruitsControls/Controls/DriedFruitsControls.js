58f56d6c7a77f21011796836acc142eeb7c130bc
Your Name <you@example.com>
2021-04-07 14:51:09 +0600

    added new files

--- src/components/DriedFruitsBuilder/DriedFruitsControls/Controls/DriedFruitsControls.js
+++ src/components/DriedFruitsBuilder/DriedFruitsControls/Controls/DriedFruitsControls.js
@@ -1,9 +1,14 @@
 import Control from "../Control/Control";
 import classes from "./DriedFruitsControls.module.css";;
 
+
 const DriedFruitsControls = ({ ingredients, addIngredient, removeIngredient }) => {
   const results = [];
+  let total = 0;
   for (const ingredient in ingredients) {
+    // Add ingredient number to totals number
+    total += ingredients[ingredient];
+    // Render pizza control for this ingredient
     results.push(<Control
         key={ingredient}
         add={addIngredient}
@@ -12,9 +17,10 @@ const DriedFruitsControls = ({ ingredients, addIngredient, removeIngredient }) =
   }
 
   return (
-    <div className={classes.DriedFruitsControls}>
+    <div className={classes.Controls}>
       <strong>Ingredients</strong>
       {results}
+      <button disabled={!total}>Order</button>
     </div>
   );
 }
