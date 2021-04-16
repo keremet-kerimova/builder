a1a839b77af82e09a94aa3787ce52d110abefbc0
Your Name <you@example.com>
2021-04-16 14:50:15 +0600

    Added router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import Layout from "./components/Layout/Layout";
 import DriedFruitsBuilder from "./components/DriedFruitsBuilder/Builder/DriedFruitsBuilder";
 import "./App.css"
+import Checkout from "./components/Checkout/Checout";
+import { Redirect, Route, Switch } from "react-router";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <DriedFruitsBuilder />
+        <Switch>
+        <Route path="/" component={DriedFruitsBuilder} exact />
+        <Route path="/checkout" component={Checkout}  />
+        <Redirect to="/" />
+        </Switch>
+        
+        
       </Layout>
     </div>
   );
--- src/components/Nav/NavItem/NavItem.js
+++ src/components/Nav/NavItem/NavItem.js
@@ -1,12 +1,14 @@
 import classes from "./NavItem.module.css";
 
-const NavItem = ({ url, children, active }) => {
+import { NavLink } from "react-router-dom";
+
+
+const NavItem = ({ url, children, exact }) => {
   return (
     <li className={classes.NavItem}>
-      <a href={url} className={active ? classes.active : null}>{children}</a>
+      <NavLink to={url} activeClassName={classes.active} exact>{children}</NavLink>
     </li>
   );
 }
 
 export default NavItem;
-
