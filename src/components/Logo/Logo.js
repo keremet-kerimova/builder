36951d13bf183ab9d323a7276da81160c2de0af0
Your Name <you@example.com>
2021-05-14 13:33:28 +0600

    All styles

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ const Logo = () => {
     return(
         <div className ={classes.Logo}>
         <img src={logo} alt="Logo of the Dried fruits project"/>
-        <div>Dry Fruits Builder</div>
+        <div className={ classes.Fonts}>Dry Fruits Builder</div>
         </div>
     );
 }
