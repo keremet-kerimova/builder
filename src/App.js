ff3f8d8f84f10f98b1d70e7d8d0e54f16311e722
Your Name <you@example.com>
2021-05-20 15:36:33 +0600

    all chnages about new components and few styles

--- src/App.js
+++ src/App.js
@@ -3,18 +3,45 @@ import DriedFruitsBuilder from "./components/DriedFruitsBuilder/Builder/DriedFru
 import Checkout from "./components/Checkout/Checkout";
 import "./App.css";
 import { Redirect, Route, Switch } from "react-router";
-import Orders from "./components/Orders/Orders"
+import Orders from "./components/Orders/Orders";
+import Auth from "./components/Auth/Auth";
+import Logout from "./components/Logout/Logout";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { restore } from "./store/actions/auth";
+
 
 const App = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(state => state.auth.token !== null);
+
+  useEffect(() => restore(dispatch), [dispatch]);
+
+  let routesOutput = (
+    <Switch>
+      <Route path="/" exact component={DriedFruitsBuilder} />
+      <Route path="/auth" component={Auth} />
+      <Redirect to="/" />
+    </Switch>
+  );
+  if (isAuthenticated) {
+    routesOutput = (
+      <Switch>
+        <Route path="/" exact component={DriedFruitsBuilder} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/auth" component={Auth} />
+        <Route path="/logout" component={Logout} />
+        <Redirect to="/" />
+      </Switch>
+    );
+  }
+
   return (
     <div className="App">
       <Layout>
-        <Switch>
-          <Route path="/" component={DriedFruitsBuilder} exact />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Redirect to="/" />
-        </Switch>
+        {routesOutput}
       </Layout>
     </div>
   );
