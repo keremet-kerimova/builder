9b5a9bf288895ac5d1aa1c82e49a8d6da1f07eef
Your Name <you@example.com>
2021-05-24 13:58:28 +0600

    Add a new auth

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "../../axios";
 import { start, auth } from "../../store/actions/auth";
 import withAxios from "../withAxios";
