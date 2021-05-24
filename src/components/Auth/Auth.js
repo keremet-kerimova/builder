1bceb0da6a7d6eba631e61d05e09d889ce10ecf2
Your Name <you@example.com>
2021-05-24 14:07:58 +0600

    Add a new word

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from "../../axios";
 import { start, auth } from "../../store/actions/auth";
 import withAxios from "../withAxios";
